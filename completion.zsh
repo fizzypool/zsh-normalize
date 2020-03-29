e0a2d16f2874de1667356de65ae53192a93d03d4
Alessandro Molari <molari.alessandro@gmail.com>
2020-03-29 10:08:32 +0000

    Update completion.zsh

--- completion.zsh
+++ completion.zsh
@@ -1,9 +1,7 @@
-# Zsh Normalization: Completion.
-
-# Load and initialize the completion system ignoring insecure directories.
+# Load and initialize the completion system ignoring insecure directories
 autoload -Uz compinit && compinit -i
 
-## Set options.
+## Options
 
 setopt   NO_NOMATCH                    # Try to avoid the 'zsh: no matches found...'
 setopt   HASH_LIST_ALL                 # Whenever a command completion is attempted, make sure the entire command path is hashed first
@@ -16,7 +14,9 @@ setopt   AUTO_PARAM_SLASH              # If completed param is a dir, add a trai
 unsetopt MENU_COMPLETE                 # Do not autoselect the first completion entry
 unsetopt FLOW_CONTROL                  # Disable start/stop characters in shell editor
 
-## Case-insensitive (all), partial-word, and then substring completion.
+## Styles
+
+# Case-insensitive (all), partial-word, and then substring completion
 if zstyle -t ':custom:module:completion:*' case-sensitive; then
   zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
   setopt CASE_GLOB
@@ -25,11 +25,11 @@ else
   unsetopt CASE_GLOB
 fi
 
-## Use caching to make completion for commands such as dpkg and apt usable.
+# Use caching to make completion for commands such as dpkg and apt usable
 zstyle ':completion::complete:*' use-cache on
 zstyle ':completion::complete:*' cache-path "${ZDOTDIR:-$HOME}/.zcompcache"
 
-## Group matches and describe.
+# Group matches and describe
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:matches' group 'yes'
 zstyle ':completion:*:options' description 'yes'
@@ -43,63 +43,39 @@ zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' verbose yes
 
-## Don't complete unavailable commands.
+# Don't complete unavailable commands
 zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
 
-## Array completion element sorting.
+# Array completion element sorting
 zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
 
-# TODO: Want it?
-## Fuzzy match mistyped completions.
-# zstyle ':completion:*' completer _complete _match _approximate
-# zstyle ':completion:*:match:*' original only
-# zstyle ':completion:*:approximate:*' max-errors 1 numeric
-## Increase the number of errors based on the length of the typed word.
-# zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
-
-## Completions - History.
+## History
 
 zstyle ':completion:*:history-words' stop yes
 zstyle ':completion:*:history-words' remove-all-dups yes
 zstyle ':completion:*:history-words' list false
 zstyle ':completion:*:history-words' menu yes
 
-## Completions - Environmental variables.
+## Environment variables
 
 zstyle ':completion::*:(-command-|export):*' fake-parameters ${${${_comps[(I)-value-*]#*,}%%,*}:#-*-}
 
-## Completions - Hostnames.
+## Hostnames
 
-### Populate hostname completion.
+# Populate hostname completion
 zstyle -e ':completion:*:hosts' hosts 'reply=(
   ${=${=${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) 2>/dev/null)"}%%[#| ]*}//\]:[0-9]*/ }//,/ }//\[/ }
   ${=${(f)"$(cat /etc/hosts(|)(N) <<(ypcat hosts 2>/dev/null))"}%%\#*}
   ${=${${${${(@M)${(f)"$(cat ~/.ssh/config 2>/dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
 )'
 
-## Completions - Users.
+## Files
 
-# TODO: Want it?
-### Don't complete uninteresting users...
-# zstyle ':completion:*:*:*:users' ignored-patterns \
-#   adm amanda apache avahi beaglidx bin cacti canna clamav daemon \
-#   dbus distcache dovecot fax ftp games gdm gkrellmd gopher \
-#   hacluster haldaemon halt hsqldb ident junkbust ldap lp mail \
-#   mailman mailnull mldonkey mysql nagios \
-#   named netdump news nfsnobody nobody nscd ntp nut nx openvpn \
-#   operator pcap postfix postgres privoxy pulse pvm quagga radvd \
-#   rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs '_*'
-
-### ... unless we really want to.
-zstyle '*' single-ignored show
-
-## Completions - Files.
-
-### Ignore multiple entries for these completions.
+# Ignore multiple entries for these completions
 zstyle ':completion:*:(rm|kill|diff):*' ignore-line other
 zstyle ':completion:*:rm:*' file-patterns '*:all-files'
 
-## Completions - Directories.
+## Directories
 
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
@@ -107,7 +83,7 @@ zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
 zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'users' 'expand'
 zstyle ':completion:*' squeeze-slashes true
 
-## Completions - Remotes.
+## Remotes
 
 zstyle ':completion:*:(scp|rsync):*' tag-order 'hosts:-host:host hosts:-domain:domain hosts:-ipaddr:ip\ address *'
 zstyle ':completion:*:(scp|rsync):*' group-order users files all-files hosts-domain hosts-host hosts-ipaddr
@@ -117,7 +93,7 @@ zstyle ':completion:*:(ssh|scp|rsync):*:hosts-host' ignored-patterns '*(.|:)*' l
 zstyle ':completion:*:(ssh|scp|rsync):*:hosts-domain' ignored-patterns '<->.<->.<->.<->' '^[-[:alnum:]]##(.[-[:alnum:]]##)##' '*@*'
 zstyle ':completion:*:(ssh|scp|rsync):*:hosts-ipaddr' ignored-patterns '^(<->.<->.<->.<->|(|::)([[:xdigit:].]##:(#c,2))##(|%*))' '127.0.0.<->' '255.255.255.255' '::1' 'fe80::*'
 
-## Completions - Procs.
+## Processes
 
 zstyle ':completion:*:*:*:*:processes' command 'ps -u $USER -o pid,user,command -w'
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;36=0=01'
@@ -125,14 +101,7 @@ zstyle ':completion:*:*:kill:*' menu yes select
 zstyle ':completion:*:*:kill:*' force-list always
 zstyle ':completion:*:*:kill:*' insert-ids single
 
-## Completions -  Mans.
+## Manuals
 
 zstyle ':completion:*:manuals' separate-sections true
 zstyle ':completion:*:manuals.(^1*)' insert-sections true
-
-## TODO: Completions - Misc.
-
-# zstyle ':completion:*:*:mpg123:*' file-patterns '*.(mp3|MP3):mp3\ files *(-/):directories'
-# zstyle ':completion:*:*:mpg321:*' file-patterns '*.(mp3|MP3):mp3\ files *(-/):directories'
-# zstyle ':completion:*:*:ogg123:*' file-patterns '*.(ogg|OGG|flac):ogg\ files *(-/):directories'
-# zstyle ':completion:*:*:mocp:*' file-patterns '*.(wav|WAV|mp3|MP3|ogg|OGG|flac):ogg\ files *(-/):directories'
